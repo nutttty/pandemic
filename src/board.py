cb3b813a23c8aacd1552f8532e1783381181b1af
Raymond  Clay <jkrofling@gmail.com>
2016-01-04 17:47:34 -0800

    added board.  needs checking

---
+++ src/board.py
@@ -0,0 +1,59 @@
+
+
+board = dict()
+
+board['madrid'] = ['london','paris','newyork','saopaulo','algiers']
+board['montreal'] = ['chicago','washington','newyork']
+board['paris'] = ['london','madrid','algiers','milan','essen']
+board['newyork'] = ['montreal','washington','madrid','london']
+board['stpetersburg'] = ['essen','istanbul','moscow']
+board['essen'] = ['london','paris','milan','stpetersburg']
+board['sanfrancisco'] = ['tokyo','manila','losangeles','chicago']
+board['atlanta'] = ['chicago','miami','washington']
+board['chicago'] = ['sanfrancisco','losangeles','mexicocity','atlanta','montreal']
+board['washington'] = ['newyork','montreal','atlanta','miami']
+board['london'] = ['newyork','madrid','paris','essen']
+board['milan'] = ['essen','paris','istanbul']
+
+board['saopaulo'] = ['madrid','bogota','buenosaires','lagos']
+board['miami'] = ['washington','atlanta','mexicocity','bogota']
+board['johannesburg'] = ['kinshasa','khartoum']
+board['botoga'] = ['miami','mexicocity','lima','buenosaires','saopaulo']
+board['losangeles'] = ['sanfrancisco','sydney','mexicocity','chicago']
+board['mexicocity'] = ['chicago','losangeles','lima','bogota','miami']
+board['lima'] = ['mexicocity','santiago','bogota']
+board['santiago'] = ['lima']
+board['kinshasa'] = ['lagos','johannesburg','khartoum']
+board['khartoum'] = ['cairo','lagos','kinshasa','johannesburg']
+board['lagos'] = ['saopaulo','kinshasa','khartoum']
+board['buenosaires'] = ['bogota','saopaulo']
+
+board['baghdad'] = ['istanbul','cairo','riyadh','karachi','tehran']
+board['kolkata'] = ['delhi','chennai','bangkok','hongkong']
+board['istanbul'] = ['milan','algiers','cairo','baghdad','moscow','stpetersburg']
+board['karachi'] = ['tehran','baghdad','riyadh','mumbai','delhi']
+board['algiers'] = ['paris','madrid','cairo','istanbul']
+board['cairo'] = ['algiers','khartoum','riyadh','baghdad','istanbul']
+board['mumbai'] = ['karachi','chennai','delhi']
+board['delhi'] = ['tehran','karachi','mumbai','chennai','kolkata']
+board['moscow'] = ['stpetersburg','istanbul','tehran']
+board['riyadh'] = ['cairo','karachi','baghdad']
+board['chennai'] = ['delhi','mumbai','jakarta','bangkok','kolkata']
+board['tehran'] = ['moscow','baghdad','karachi','delhi']
+
+board['taipei'] = ['shanghai','hongkong','manila','osaka']
+board['beijing'] = ['shanghai','seoul']
+board['hochiminhcity'] = ['bangkok','jakarta','manila','hongkong']
+board['tokyo'] = ['seoul','shanghai','osaka','sanfrancisco']
+board['manila'] = ['taipei','hongkong','hochiminhcity','sydney','sanfrancisco']
+board['hongkong'] = ['kolkata','bangkok','hochiminhcity','manila','taipei','shanghai']
+board['bankok'] = ['kolkata','chennai','jakarta','hochiminhcity','hongkong']
+board['seoul'] = ['beijing','shanghai','tokyo']
+board['osaka'] = ['tokyo','taipei']
+board['jakarta'] = ['chennai','sydney','hochiminhcity','bangkok']
+board['shangai'] = ['beijing','hongkong','taipei','tokyo','seoul']
+board['sydney'] = ['manila','jakarta','losangeles']
+
+def boardchecker():
+	pass
+
